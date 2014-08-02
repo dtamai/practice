ed90e62281998456369c30450db0e3228bb90a65
Daniel Tamai <daniel.tamai@gmail.com>
2014-08-02 17:33:44 -0300

    Add Kata 2

--- kata/02.rb
+++ kata/02.rb
@@ -1,20 +1,143 @@
 module Kata02
   module Iterative
     def chop(x, ary)
-      2
+      return -1 if ary.empty?
+      return -1 if x < ary[0] || x > ary[-1]
+
+      offset = -1
+      while ary.length > 1
+        min = 0
+        max = ary.length - 1
+        mid = max/2
+
+        if x > ary[mid]
+          offset += mid + 1
+          ary = ary.slice (mid + 1)..max
+        else
+          ary = ary.slice min..mid
+        end
+
+      end
+      x == ary[0] ? offset + 1 : -1
+    end
+
+    def l(min, mid, max, offset, ary)
+      puts "min: #{min} mid: #{mid} max: #{max} offset: #{offset} ary: #{ary}"
     end
   end
 
   module Recursive
+    def chop(x, ary)
+      binary_search(x, ary, 0, ary.length - 1)
+    end
+
+    def binary_search(x, ary, min, max)
+      return -1 if min > max
+
+      mid = (min + max)/2
+
+      if x < ary[mid]
+        return binary_search(x, ary, min, mid-1)
+      elsif x > ary[mid]
+        return binary_search(x, ary, mid+1, max)
+      else
+        return mid
+      end
+    end
   end
 
   module Functional
+    def chop(x, ary)
+      binary_search(x, ary, 0)
+    end
+
+    def binary_search(x, ary, offset)
+      return -1 if ary.empty?
+      return -1 if x < ary[0] || x > ary[-1]
+
+      mid = (ary.length - 1)/2
+      if x < ary[mid]
+        return binary_search(x, ary.slice(0..mid), offset)
+      elsif x > ary[mid]
+        return binary_search(x, ary.slice((mid+1)..-1), offset+mid+1)
+      else
+        return offset + mid
+      end
+    end
   end
 
   module Parallel
+    def chop(x, ary)
+      return -1 if ary.empty?
+      parallel_search(x, ary, 0, ary.length - 1)
+    end
+
+    def parallel_search(x, ary, min, max)
+      return -1 if min > max
+
+      mid = (min + max)/2
+      return mid if x == ary[mid]
+
+      lo = Thread.new { parallel_search(x, ary, min, mid-1) }
+      hi = Thread.new { parallel_search(x, ary, mid+1, max) }
+
+      if lo.value > -1
+        return lo.value
+      else
+        return hi.value
+      end
+    end
   end
 
-  module Metaprogramming
+  module Unnecessary
+    def chop(x, ary)
+      return -1 if ary.empty?
+      @split = Split.new(ary, 0, ary.length - 1) unless ary.equal? @ary
+      @ary = ary
+      @split.find(x)
+    end
+
+    class Split
+      def initialize(ary, min_idx, max_idx)
+        mid_idx = (min_idx + max_idx)/2
+
+        if min_idx < max_idx
+          @left_down = Split.new(ary, min_idx, mid_idx)
+          @right_down = Split.new(ary, mid_idx+1, max_idx)
+        else
+          @left_down = SplitTerminal.new(mid_idx)
+          @right_down = SplitTerminal.new(mid_idx+1)
+        end
+        @left_range = ary[min_idx]..ary[mid_idx]
+        @right_range = ary[mid_idx]..ary[max_idx]
+      end
+
+      def find(x)
+        case x
+        when @left_range then left(x)
+        when @right_range then right(x)
+        else -1
+        end
+      end
+
+      def left(x)
+        @left_down.find(x)
+      end
+
+      def right(x)
+        @right_down.find(x)
+      end
+    end
+
+    class SplitTerminal
+      def initialize(idx)
+        @idx = idx
+      end
+
+      def find(x)
+        @idx
+      end
+    end
   end
 end
 
@@ -47,8 +170,32 @@ module ChopAssertion
   end
 end
 
-class TestKata02 < Minitest::Test
+class TestKata02_Iterative < Minitest::Test
   include ChopAssertion
   include Kata02::Iterative
+end
+
+class TestKata02_Recursive < Minitest::Test
+  include ChopAssertion
+  include Kata02::Recursive
+end
+
+class TestKata02_Functional < Minitest::Test
+  include ChopAssertion
+  include Kata02::Functional
+end
+
+class TestKata02_Parallel < Minitest::Test
+  include ChopAssertion
+  include Kata02::Parallel
+end
+
+class TestKata02_Unnecessary < Minitest::Test
+  include ChopAssertion
+  include Kata02::Unnecessary
+
+  def setup
+    @split = nil
+  end
 end
 
